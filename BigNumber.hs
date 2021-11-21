3b0368659875522e4f1346402a107513811f7633
José Pedro Ferreira <up201904515@fe.up.pt>
2021-11-21 01:11:00 +0000

    Added SumBN and SubBN. Plus many aux functions

--- BigNumber.hs
+++ BigNumber.hs
@@ -19,8 +19,8 @@ nDigits n
 
 
 buildBigN :: Bool -> Int -> [Int] -> BigNumber
-buildBigN b n l 
-  | nDigits n == 1 = BigNumber b (n:l)
+buildBigN b n l
+  | nDigits n == 1 = BigNumber b ((abs n):l)
   | otherwise = buildBigN b (div (abs n) 10) (mod (abs n) 10 : l)
 
 intToBigN :: Int -> BigNumber
@@ -28,7 +28,7 @@ intToBigN n
   | n < 0 = buildBigN False n []
   | otherwise = buildBigN True n []
 
-instance Show BigNumber where 
+instance Show BigNumber where
   show (BigNumber b l)
     | b = "+" ++ show l
     | otherwise = "-" ++ show l
@@ -36,18 +36,27 @@ instance Show BigNumber where
 getBigNSize :: BigNumber -> Int
 getBigNSize bn = length (digits bn)
 
+instance Ord BigNumber where
+  compare (BigNumber s1 l1) (BigNumber s2 l2)
+    | s1 && (not s2) = GT
+    | (not s1) && s2 = LT
+    | length zdl1 /= length zdl2 = compare (length zdl1) (length zdl2)
+    | otherwise =  compare zdl1 zdl2
+    where zdl1 = (digits (zeroDestuffing (BigNumber s1 l1)))
+          zdl2 = (digits (zeroDestuffing (BigNumber s2 l2)))
+
 {-
-instance Show BigNumber where 
+instance Show BigNumber where
   show (BigNumber b l)
     | b = show l
     | otherwise = "-" ++ show l
     -}
-  
+
 scanner :: String -> BigNumber
 scanner s = intToBigN (read s::Int)
 
 bigNToString :: BigNumber -> String -> String
-bigNToString bn s 
+bigNToString bn s
   | getBigNSize bn == 1 = s ++ show (head (digits bn))
   | otherwise = bigNToString (BigNumber (sign bn) (tail (digits bn))) (s ++ show (head (digits bn)))
 
@@ -59,11 +68,67 @@ output bn
 
 zeroStuffing :: BigNumber -> Int -> BigNumber
 zeroStuffing bn nz
+  | nz <= 0 = bn
   | nz == 1 = BigNumber (sign bn) (0 : digits bn)
   | otherwise = zeroStuffing (BigNumber (sign bn) (0 : digits bn)) (nz-1)
 
+zeroStuffingExact :: BigNumber -> Int ->BigNumber
+zeroStuffingExact bn nz = zeroStuffing bn (nz - (length (digits bn)))
+
+zeroDestuffing :: BigNumber -> BigNumber
+zeroDestuffing bn
+  | length (digits bn) == 1 = bn
+  | head (digits bn) == 0 = zeroDestuffing (BigNumber (sign bn) (tail (digits bn)))
+  | otherwise = bn
+
+absBn :: BigNumber -> BigNumber
+absBn bn = BigNumber True (digits bn)
+
+negativeBn :: BigNumber -> BigNumber
+negativeBn bn = BigNumber False (digits bn)
+
+simBn :: BigNumber -> BigNumber
+simBn bn = BigNumber (not (sign bn)) (digits bn)
+
+rawSumBn :: BigNumber -> BigNumber -> BigNumber
+rawSumBn bn1 bn2
+  | (sign bn1) && (sign bn2) = BigNumber True (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
+  | (not (sign bn1)) && (not (sign bn2)) = BigNumber False (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
+  | (sign bn1) && (not (sign bn2)) = rawSubBn bn1 (absBn bn2)
+  | (not (sign bn1)) && (sign bn2) = rawSubBn bn2 (absBn bn1)
+  where nz = (max (length (digits bn1)) (length (digits bn2))) + 1
+
+rawSubBn :: BigNumber -> BigNumber -> BigNumber
+rawSubBn bn1 bn2
+  | bn1 == bn2 = (scanner "0")
+  | (sign bn1) && (sign bn2) = (if bn1 < bn2
+    then negativeBn (rawSubBn bn2 bn1)
+    else (BigNumber True (zipWith (-) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz))) ))
+  | (not (sign bn1)) && (not (sign bn2)) = rawSubBn (absBn bn2) (absBn bn1)
+  | (sign bn1) && (not (sign bn2)) = rawSumBn bn1 (absBn bn2)
+  | (not (sign bn1)) && (sign bn2) = negativeBn (rawSumBn (absBn bn1) bn2)
+  where nz = (max (length (digits bn1)) (length (digits bn2))) + 1
+
+
 carrySum :: Int -> Int -> (Int, Int)
 carrySum n1 n2 = (div (n1 + n2) 10 ,mod (n1 + n2) 10)
+
+processOperation :: BigNumber -> BigNumber
+processOperation bn = zeroDestuffing (BigNumber (sign bn) (processOperationCarry (digits bn) 0 []))
+
+sumBN :: BigNumber -> BigNumber ->BigNumber
+sumBN bn1 bn2 = processOperation (rawSumBn bn1 bn2)
+
+subBN :: BigNumber -> BigNumber ->BigNumber
+subBN bn1 bn2 = processOperation (rawSubBn bn1 bn2)
+
+processOperationCarry :: [Int] -> Int -> [Int] -> [Int]
+processOperationCarry l carry res
+  | length l == 0 = res
+  | (current >= 0 && current <= 9) = processOperationCarry (reverse(tail (reverse l))) 0 ([current]++res)
+  | (current >= 10) = processOperationCarry (reverse(tail (reverse l))) 1 ([(mod current 10)]++res)
+  | (current < 0) = processOperationCarry (reverse(tail (reverse l))) (-1) ([(current + 10)]++res)
+  where current = (last l) + carry
 
 {-
 sumTuplesToBigN :: [(Int, Int)] -> BigNumber -> BigNumber
@@ -77,4 +142,3 @@ sumTuplesToBigN l bn
 
 parseSumTuples :: [(Int, Int)] -> BigNumber
 parseSumTuples l = -}
-
