98a558f57ba13b3eb2f98d778e96318ffbe03f5e
José Pedro Ferreira <up201904515@fe.up.pt>
2021-11-21 23:40:19 +0000

    Added mulBN and other aux functions

--- BigNumber.hs
+++ BigNumber.hs
@@ -18,23 +18,23 @@ nDigits n
   | otherwise = 1
 
 
-buildBigN :: Bool -> Int -> [Int] -> BigNumber
-buildBigN b n l
+buildBN :: Bool -> Int -> [Int] -> BigNumber
+buildBN b n l
   | nDigits n == 1 = BigNumber b ((abs n):l)
-  | otherwise = buildBigN b (div (abs n) 10) (mod (abs n) 10 : l)
+  | otherwise = buildBN b (div (abs n) 10) (mod (abs n) 10 : l)
 
-intToBigN :: Int -> BigNumber
-intToBigN n
-  | n < 0 = buildBigN False n []
-  | otherwise = buildBigN True n []
+intToBN :: Int -> BigNumber
+intToBN n
+  | n < 0 = buildBN False n []
+  | otherwise = buildBN True n []
 
 instance Show BigNumber where
   show (BigNumber b l)
     | b = "+" ++ show l
     | otherwise = "-" ++ show l
 
-getBigNSize :: BigNumber -> Int
-getBigNSize bn = length (digits bn)
+getBNSize :: BigNumber -> Int
+getBNSize bn = length (digits bn)
 
 instance Ord BigNumber where
   compare (BigNumber s1 l1) (BigNumber s2 l2)
@@ -53,17 +53,17 @@ instance Show BigNumber where
     -}
 
 scanner :: String -> BigNumber
-scanner s = intToBigN (read s::Int)
+scanner s = intToBN (read s::Int)
 
-bigNToString :: BigNumber -> String -> String
-bigNToString bn s
-  | getBigNSize bn == 1 = s ++ show (head (digits bn))
-  | otherwise = bigNToString (BigNumber (sign bn) (tail (digits bn))) (s ++ show (head (digits bn)))
+convertBNToString :: BigNumber -> String -> String
+convertBNToString bn s
+  | getBNSize bn == 1 = s ++ show (head (digits bn))
+  | otherwise = convertBNToString (BigNumber (sign bn) (tail (digits bn))) (s ++ show (head (digits bn)))
 
 output :: BigNumber -> String
 output bn
-  | sign bn = bigNToString bn ""
-  | otherwise = bigNToString bn "-"
+  | sign bn = convertBNToString bn ""
+  | otherwise = convertBNToString bn "-"
 
 
 zeroStuffing :: BigNumber -> Int -> BigNumber
@@ -78,35 +78,45 @@ zeroStuffingExact bn nz = zeroStuffing bn (nz - (length (digits bn)))
 zeroDestuffing :: BigNumber -> BigNumber
 zeroDestuffing bn
   | length (digits bn) == 1 = bn
-  | head (digits bn) == 0 = zeroDestuffing (BigNumber (sign bn) (tail (digits bn)))
+  | head (digits bn) <= 0 = zeroDestuffing (BigNumber (sign bn) (tail (digits bn)))
   | otherwise = bn
 
-absBn :: BigNumber -> BigNumber
-absBn bn = BigNumber True (digits bn)
+absBN :: BigNumber -> BigNumber
+absBN bn = BigNumber True (digits bn)
 
-negativeBn :: BigNumber -> BigNumber
-negativeBn bn = BigNumber False (digits bn)
+negativeBN :: BigNumber -> BigNumber
+negativeBN bn = BigNumber False (digits bn)
 
-simBn :: BigNumber -> BigNumber
-simBn bn = BigNumber (not (sign bn)) (digits bn)
+simBN :: BigNumber -> BigNumber
+simBN bn = BigNumber (not (sign bn)) (digits bn)
 
-rawSumBn :: BigNumber -> BigNumber -> BigNumber
-rawSumBn bn1 bn2
-  | (sign bn1) && (sign bn2) = BigNumber True (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
-  | (not (sign bn1)) && (not (sign bn2)) = BigNumber False (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
-  | (sign bn1) && (not (sign bn2)) = rawSubBn bn1 (absBn bn2)
-  | (not (sign bn1)) && (sign bn2) = rawSubBn bn2 (absBn bn1)
+
+zipWithBN :: Bool -> BigNumber -> BigNumber -> (Int->Int->Int) -> BigNumber
+zipWithBN s bn1 bn2 op = zeroDestuffing (BigNumber s (zipWith (op) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz))) )
   where nz = (max (length (digits bn1)) (length (digits bn2))) + 1
 
-rawSubBn :: BigNumber -> BigNumber -> BigNumber
-rawSubBn bn1 bn2
+
+mapBN :: Bool -> BigNumber -> (Int->Int) -> BigNumber
+mapBN s bn op = BigNumber s (map (op) (digits bn))
+--mapBN s bn op = zeroDestuffing (BigNumber s (map (op) (digits (zeroStuffing bn 1))))
+
+rawSumBN :: BigNumber -> BigNumber -> BigNumber
+rawSumBN bn1 bn2
+  | (sign bn1) && (sign bn2) = BigNumber True (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
+  | (not (sign bn1)) && (not (sign bn2)) = BigNumber False (zipWith (+) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz)) )
+  | (sign bn1) && (not (sign bn2)) = rawSubBN bn1 (absBN bn2)
+  | (not (sign bn1)) && (sign bn2) = rawSubBN bn2 (absBN bn1)
+  where nz = (max (length (digits bn1)) (length (digits bn2))) + 1
+
+rawSubBN :: BigNumber -> BigNumber -> BigNumber
+rawSubBN bn1 bn2
   | bn1 == bn2 = (scanner "0")
   | (sign bn1) && (sign bn2) = (if bn1 < bn2
-    then negativeBn (rawSubBn bn2 bn1)
+    then negativeBN (rawSubBN bn2 bn1)
     else (BigNumber True (zipWith (-) (digits (zeroStuffingExact bn1 nz)) (digits (zeroStuffingExact bn2 nz))) ))
-  | (not (sign bn1)) && (not (sign bn2)) = rawSubBn (absBn bn2) (absBn bn1)
-  | (sign bn1) && (not (sign bn2)) = rawSumBn bn1 (absBn bn2)
-  | (not (sign bn1)) && (sign bn2) = negativeBn (rawSumBn (absBn bn1) bn2)
+  | (not (sign bn1)) && (not (sign bn2)) = rawSubBN (absBN bn2) (absBN bn1)
+  | (sign bn1) && (not (sign bn2)) = rawSumBN bn1 (absBN bn2)
+  | (not (sign bn1)) && (sign bn2) = negativeBN (rawSumBN (absBN bn1) bn2)
   where nz = (max (length (digits bn1)) (length (digits bn2))) + 1
 
 
@@ -117,10 +127,10 @@ processOperation :: BigNumber -> BigNumber
 processOperation bn = zeroDestuffing (BigNumber (sign bn) (processOperationCarry (digits bn) 0 []))
 
 sumBN :: BigNumber -> BigNumber ->BigNumber
-sumBN bn1 bn2 = processOperation (rawSumBn bn1 bn2)
+sumBN bn1 bn2 = processOperation (rawSumBN bn1 bn2)
 
 subBN :: BigNumber -> BigNumber ->BigNumber
-subBN bn1 bn2 = processOperation (rawSubBn bn1 bn2)
+subBN bn1 bn2 = processOperation (rawSubBN bn1 bn2)
 
 processOperationCarry :: [Int] -> Int -> [Int] -> [Int]
 processOperationCarry l carry res
@@ -129,6 +139,25 @@ processOperationCarry l carry res
   | (current >= 10) = processOperationCarry (reverse(tail (reverse l))) 1 ([(mod current 10)]++res)
   | (current < 0) = processOperationCarry (reverse(tail (reverse l))) (-1) ([(current + 10)]++res)
   where current = (last l) + carry
+
+raiseTenBN :: BigNumber -> Int -> BigNumber
+raiseTenBN bn power
+  | power <= 0 = bn
+  | power == 1 = BigNumber (sign bn) ((digits bn)++[0])
+  | otherwise = raiseTenBN (BigNumber (sign bn) ((digits bn)++[0])) (power - 1)
+
+rawMulBN :: BigNumber -> BigNumber -> BigNumber -> Int -> BigNumber
+rawMulBN bn mul_bn res_bn power
+  | bn == scanner "0" || mul_bn == scanner "0" = scanner "0"
+  | (not (sign bn)) && (not (sign mul_bn)) = BigNumber True (digits (rawMulBN (absBN bn) (absBN mul_bn) res_bn power))
+  | (sign bn) /= (sign mul_bn) = BigNumber False (digits (rawMulBN (absBN bn) (absBN mul_bn) res_bn power))
+  | absBN mul_bn > absBN bn = rawMulBN mul_bn bn res_bn power
+  | getBNSize mul_bn == 0 = res_bn
+  | getBNSize mul_bn == 1 = sumBN res_bn (raiseTenBN (processOperation(mapBN True bn (*(last (digits mul_bn))))) power)
+  | otherwise = rawMulBN bn (BigNumber (sign mul_bn) (reverse(tail (reverse (digits mul_bn))))) (sumBN res_bn (raiseTenBN (processOperation(mapBN True bn (*(last (digits mul_bn))))) power)) (power + 1)
+
+mulBN :: BigNumber -> BigNumber -> BigNumber
+mulBN bn1 bn2 = zeroDestuffing (rawMulBN bn1 bn2 (scanner "0") 0)
 
 {-
 sumTuplesToBigN :: [(Int, Int)] -> BigNumber -> BigNumber
